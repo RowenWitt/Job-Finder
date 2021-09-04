7d627151b0c9ec96eb7344f379cc61dd3500cdb9
RowenWitt <rew4200@protonmail.com>
2021-09-03 17:39:57 -0700

    adds main.py

---
+++ app/main.py
@@ -0,0 +1,17 @@
+from dotenv import load_dotenv, find_dotenv
+load_dotenv(find_dotenv)
+
+import os
+
+import app.scraper
+import app.db
+
+# To be passed in as arguments
+root_url = os.getenv('ROOT_URL')
+driver_path = os.getenv('DRIVER_PATH')
+
+scraper = app.scraper.scraper(root_url, driver_path)
+
+data = scraper.scrape()
+
+print(data)
