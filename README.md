054388a2c6046cf79ee8d6ada83756e44c7a3e53
RowenWitt <rew4200@protonmail.com>
2021-09-03 17:22:18 -0700

    adds basic functionality to db.py

--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 Scraper, ETL Pipeline, API for getting jobs from Indeed
 
 - ChromeDriver version `93.0.4577.15`
+- Python version `3.9.0`
 
 ## Known Defects
 
@@ -10,5 +11,7 @@ Scraper, ETL Pipeline, API for getting jobs from Indeed
 - Date posted is being stored in the database as the date of scraping minus the 'days since posted' from Indeed.  After 30 days this counter just says 'more than 30 days', this means that any job older than 30 days could be any number of days older than 30.  There's probably a better way
 
 - Some job posts store descriptions in slightly different div location, could be managed through through an if statement
+
+- reviews sometimes doesn't show up when it should, likely due to uncommon html structure
 
 - If you want to build this into an app, you'll need a table to handle login info as well as a table relating `id` from JobListings table to a user's related actions.
--- app/db.py
+++ app/db.py
@@ -9,3 +9,68 @@ from sqlalchemy.orm import sessionmaker, scoped_session
 
 from app.models import JobListings
 
+db_url = os.getenv('DB_URL')
+
+class Database(object):
+
+
+	def __init__(self):
+		self.engine = create_engine(
+			db_url,
+			pool_recycle=3600,
+			pool_size=10,
+			echo=False,
+			pool_pre_ping=True
+		)
+
+		self.Sessionmaker = scoped_session(
+			sessionmaker(
+				autoflush=False,
+				autocommit=False,
+				bind=self.engine
+			)
+		)
+
+
+	def init_JobListings(self):
+		""" initializes JobListings table if not exists """
+
+		insp = inspect(self.engine)
+		if insp.has_table('job_listings') == False:
+			JobListings.__table__.create(self.engine)
+
+
+	def insert_JobListings(self, data: List[Dict]):
+		""" inserts data into JobListings """
+		with self.Sessionmaker() as session:
+			last = select(func.max(JobListings.id))
+			last_value = session.execute(last).fetchall()[0][0]
+			for i in range(len(data)):
+				if last_value is None:
+					last_value = 0
+				last_value += 1
+				data[i]['id'] = last_value
+				obj = JobListings(**data[i])
+				session.add(obj)
+				session.commit(obj)
+
+
+	def get_all_JobListings(self):
+		""" SELECT * FROM job_listings """
+		with self.Sessionmaker() as session:
+			query = select(JobListings)
+			data = session.execute(query).fetchall()
+
+		return data
+
+
+
+	def get_after_date_JobListings(self, date):
+		""" SELECT * FROM job_listings WHERE date > input """
+		with self.Sessionmaker() as session:
+			query = select(JobListings).
+				where(JobListings.date >= date)
+			data = session.execute(query)
+
+		return data
+
